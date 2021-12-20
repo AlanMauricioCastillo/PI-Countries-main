39e16525c5f8cf64a4563a7c12ff8653b44ebde9
Mauricio Alan Castillo <79920621+AlanMauricioCastillo@users.noreply.github.com>
2021-12-20 18:30:51 -0300

    test 1 change http from https on variables.js file

--- api/src/Variables.js
+++ api/src/Variables.js
@@ -1,7 +1,7 @@
 const URL = {
-  COUNTRIES: "https://restcountries.com/v3/all",
-  COUNTRIES_NAME: "https://restcountries.com/v3/name/", //{name}
-  COUNTRIES_CODE: "https://restcountries.com/v3/alpha/", //{code}
+  COUNTRIES: "http://restcountries.com/v3/all",
+  COUNTRIES_NAME: "http://restcountries.com/v3/name/", //{name}
+  COUNTRIES_CODE: "http://restcountries.com/v3/alpha/", //{code}
 };
 
 module.exports = {
