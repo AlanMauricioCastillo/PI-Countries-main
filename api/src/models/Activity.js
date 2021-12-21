d09b060601f58211db03f397a2cc7d5eadd8f567
Mauricio Alan Castillo <79920621+AlanMauricioCastillo@users.noreply.github.com>
2021-12-20 21:17:16 -0300

    test 2 cambie nombre de tablas y force false

--- api/src/models/Activity.js
+++ api/src/models/Activity.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   sequelize.define(
-    "Activity",
+    "activity",
     {
       id: {
         type: DataTypes.INTEGER,
--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   sequelize.define(
-    "Country",
+    "country",
     {
       id: {
         type: DataTypes.STRING,
